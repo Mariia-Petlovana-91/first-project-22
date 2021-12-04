5dfee56bdda0d53c0e112772bbcd85a05281262a
h0wter <91832203+h0wter@users.noreply.github.com>
2021-12-04 11:48:52 +0000

    Deploying to gh-pages from @ h0wter/first-project-22@74043f4846261f4ad4f7603dcab6818df25e0d5c 🚀

--- src.fbf7d74b.js
+++ src.fbf7d74b.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/first-project-22/src.0a9032fc.js.map
+//# sourceMappingURL=/first-project-22/src.fbf7d74b.js.map
